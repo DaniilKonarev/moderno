99beceae84488798fa2eda84313b4c360f211098
DaniilKonarev <danek32@mail.ru>
2021-06-28 21:53:04 +0300

    create slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 	var mixer = mixitup('.products__inner-box');
 
-	
+	$(".rate-start").rateYo({
+		rating: 5,
+		starWidth: "12px",
+		readOnly: true,
+	 });
+
+
+	 $('.product-slider__inner').slick({
+		 dots: true,
+		 slidesToShow: 4,
+		 arrows: false,
+		 slidesToScroll: 4,
+	 });
+
 });
